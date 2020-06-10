f9e83a83f7bc0c300f5fb1180825fdb9345492d4
wangyaru <wangyaru@yidian-inc.com>
2020-06-10 16:33:12 +0800

    add qrcode

--- src/views/test/upload.vue
+++ src/views/test/upload.vue
@@ -16,18 +16,28 @@
         src="">      
       </vue-cropper>
     </div>
+    <div class="qrcode-wrapper">
+      <div class="qrcode-box">
+        <canvas class="qrcode-canvas" ref="qrcode"></canvas>
+      </div>
+      <div class="btn-wrapper">
+        <el-input size="mini" v-model="bannerUrl"></el-input>
+        <el-button size="mini" @click="createQrc">Click!!</el-button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import VueCropper from 'vue-cropperjs';
-
+import QRCode from 'qrcode';
 export default {
   name: 'VUpload',
   componentName: 'VUpload',
   data(){
     return {
       imgStyle: {height: '300px'},
-      cropperVisible: false
+      cropperVisible: false,
+      bannerUrl: ''
     }
   },
   methods: {
@@ -51,6 +61,28 @@ export default {
         this.$error('FileReader API not supported')
       }
     },
+    createQrc(){
+      if(!this.bannerUrl){
+        this.$message({
+          message: 'url is needed!!',
+          type: 'warning'
+        });
+        return false
+      }
+      QRCode.toCanvas(this.$refs.qrcode, this.bannerUrl, (error) => {
+        if(error) {
+          this.$message({
+            message: error,
+            type: 'warning'
+          });
+        } else {
+          this.$message({
+            message: 'success',
+            type: 'success'
+          })
+        }
+      })
+    }
   },
   components: {
     VueCropper
@@ -70,6 +102,14 @@ export default {
   }
   .el-button--primary:hover{
     background: #234480;
+  }
+}
+.qrcode-wrapper{
+  .btn-wrapper .el-input{
+    width: 180px !important;
+  }
+  .btn-wrapper button{
+    display:inline-block
   }
 }
 
